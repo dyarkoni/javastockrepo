80ed0a0db35e51c473659dc2f630719b1204534b
dyarkoni <danayarkoni@gmail.com>
2014-12-23 02:56:17 +0200

    Exercise 2

--- src/dana/myorg/java/DanaproServlet.java
+++ src/dana/myorg/java/DanaproServlet.java
@@ -3,9 +3,17 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
+
 public class DanaproServlet extends HttpServlet {
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		resp.setContentType("text/html");
+		int num1=4;
+		int num2=7;
+		int num3=3;
+		int result=(num1+num3)*num2;
+		String resultStr=new String("<h2 style=color:green>Result of: "+"("+num1+"+"+num3+")"+"*"+num2+"="+result+"</h2>");
+		resp.getWriter().println(resultStr);
 	}
 }
